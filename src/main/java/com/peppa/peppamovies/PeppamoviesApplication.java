0aac63d01396539766dd282952ea2a81a36e2e41
xialin219 <xia.lin@stonybrook.edu>
2018-03-29 20:53:58 -0400

    Initial Commit by Lindsey.

---
+++ src/main/java/com/peppa/peppamovies/PeppamoviesApplication.java
@@ -0,0 +1,12 @@
+package com.peppa.peppamovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeppamoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeppamoviesApplication.class, args);
+	}
+}
