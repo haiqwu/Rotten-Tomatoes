aa123d61581e899402fed74e053a888b6d2f85be
HaiqiWu219 <haiqi.wu@stonybrook.edu>
2018-04-09 01:14:26 -0400

    1/3 use cases done - Haiqi

--- src/main/java/com/peppa/peppamovies/model/MovieRankingData.java
+++ src/main/java/com/peppa/peppamovies/model/MovieRankingData.java
@@ -2,6 +2,7 @@ package com.peppa.peppamovies.model;
 
 import java.util.ArrayList;
 
+
 public class MovieRankingData {
     private ArrayList<MovieInfo> moviesOpeningThisWeek;
     private ArrayList<MovieInfo> topBoxMovies;
@@ -10,6 +11,17 @@ public class MovieRankingData {
     private ArrayList<MovieInfo> topMovies;
     private ArrayList<MovieInfo> rollingBarMovies;
     private MovieInfo webBackgroundMovie;
+
+
+    public MovieRankingData() {
+        moviesOpeningThisWeek = new ArrayList<MovieInfo>();
+        topBoxMovies= new ArrayList<MovieInfo>();
+        comingSoonMovies =  new ArrayList<MovieInfo>();
+        certifiedFreshMovies= new ArrayList<MovieInfo>();
+        topMovies = new ArrayList<MovieInfo>();
+        rollingBarMovies= new ArrayList<MovieInfo>();
+        webBackgroundMovie = new MovieInfo();
+    }
 
     public ArrayList<MovieInfo> getMoviesOpeningThisWeek() {
         return moviesOpeningThisWeek;
@@ -65,5 +77,25 @@ public class MovieRankingData {
 
     public void setWebBackgroundMovie(MovieInfo webBackgroundMovie) {
         this.webBackgroundMovie = webBackgroundMovie;
+    }
+
+    public ArrayList<long[]> loadMovieRankingData()
+    {
+        ArrayList<long[]> arr = new ArrayList<long[]>();
+
+
+        long[] top_box_office_list = {1309, 2005, 2032, 2006, 2033, 733, 2034, 2035 };//  2007, 2036
+        long[] comming_soon = {2009, 2020, 2023, 2024, 2025, 2026, 2027, 2028};// 2029, 2031
+        long[] opening_this_week = {2008, 2010, 2011, 2012, 2013, 2014, 2015, 2016};//, 2017, 2018
+        long[] certified_fresh_movies = {1309, 2038, 2006, 2007, 2005, 2039, 2019, 2037};//, 2030, 2003
+
+        arr.add(top_box_office_list);
+        arr.add(comming_soon);
+        arr.add(opening_this_week);
+        arr.add(certified_fresh_movies);
+
+        return arr;
+
+
     }
 }
--- src/main/java/com/peppa/peppamovies/web/controller/UserController.java
+++ src/main/java/com/peppa/peppamovies/web/controller/UserController.java
@@ -1,29 +1,78 @@
 package com.peppa.peppamovies.web.controller;
 
+import com.peppa.peppamovies.model.MovieInfo;
+import com.peppa.peppamovies.model.MovieRankingData;
 import com.peppa.peppamovies.model.UserInfo;
+import com.peppa.peppamovies.service.MovieService;
 import com.peppa.peppamovies.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
-
-
+import java.util.ArrayList;
 
 
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private MovieService movieService;
 
     @GetMapping("/")
-    public String loginPage(){
+    public String loginPage(Model model){
+        MovieRankingData movieRankingData = new MovieRankingData();
+        ArrayList<long[]> ranks =  movieRankingData.loadMovieRankingData();
+        int num_per_list = 8;
+
+        //1. get top box office movies:
+        long[] top_box_office_list =  ranks.get(0);
+
+
+        for (int i = 0; i < num_per_list; i++) {
+            MovieInfo movie = movieService.getMovie(  (Long) (top_box_office_list[i]) );
+
+            movieRankingData.getTopBoxMovies().add(movie);
+        }
+
+        //2. get comming_soon:
+        long[] comming_soon_list =  ranks.get(1);
+
+
+        for (int i = 0; i < num_per_list; i++) {
+            MovieInfo movie = movieService.getMovie(  (Long) (comming_soon_list[i]) );
+
+            movieRankingData.getComingSoonMovies().add(movie);
+        }
+        //3. get opening_this_week:
+        long[] opening_this_week_list =  ranks.get(2);
+
+
+        for (int i = 0; i < num_per_list; i++) {
+            MovieInfo movie = movieService.getMovie(  (Long) (opening_this_week_list[i]) );
+
+            movieRankingData.getMoviesOpeningThisWeek().add(movie);
+        }
+        //4. certified_fresh_movies:
+        long[] certified_fresh_movies_list =  ranks.get(3);
+
+        for (int i = 0; i < num_per_list; i++) {
+            MovieInfo movie = movieService.getMovie(  (Long) (certified_fresh_movies_list[i]) );
+
+            movieRankingData.getCertifiedFreshMovies().add(movie);
+        }
+        model.addAttribute("m1",movieRankingData.getMoviesOpeningThisWeek() );
+        model.addAttribute("m2",movieRankingData.getTopBoxMovies());
+        model.addAttribute("m3",movieRankingData.getCertifiedFreshMovies());
+        model.addAttribute("m4",movieRankingData.getComingSoonMovies());
         return "index";
     }
     @PostMapping("/login")
     public String handleLogin(@RequestParam String username, @RequestParam String password,
-                              HttpSession session, RedirectAttributes attributes){
+                              HttpSession session, RedirectAttributes attributes,Model model) {
         byte[] temp =password.getBytes();
         Byte[] passwdByte = toByteArr(temp);
 
@@ -31,7 +80,9 @@ public class UserController {
         if(user != null){
             user.setPassW(null);
             session.setAttribute("user", user);
+            model.addAttribute("user_info", user  );
             System.out.println("LoginSuccess");
+
             return "index";
         }else{
             attributes.addFlashAttribute("message", "usename and password not match");
