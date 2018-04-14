705c9bf4ee1b778eebba56128607b8e4c0ca7038
hy <haiyang.liu@stonybrook.edu>
2018-04-14 19:26:47 -0400

    HY: add check box for critics in login

--- src/main/java/com/peppa/peppamovies/PeppamoviesApplication.java
+++ src/main/java/com/peppa/peppamovies/PeppamoviesApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PeppamoviesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PeppamoviesApplication.class, args);
 	}
-
-	// Haiqi SB
 }
